ef214715bd63391e042e091ef3b0645540b50f28
Kianamartini <kiana.martini@web.de>
2026-06-14 12:30:02 +0200

    Load db password dynamically from env for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import os
+from dotenv import load_dotenv
+load_dotenc()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -59,8 +63,14 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    section = config.get_section(config.config_ini_section, {})
+    db_password = os.getenv("DB_PASSWORD")
+    if db_password:
+        section["sqlalchemy.url"] = f"postgresql://pixelwise:{db_password}@localhost/pixelwise"
+    else:
+        section["sqlalchemy.url"] = "postgresql://pixelwise@localhost/pixelwise"
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        section,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
